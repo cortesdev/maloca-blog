49934833d77f54298aa82e61250aa8baa4cf4058
Ricardo Côrtes <giralander@hotmail.com>
2023-06-17 20:11:46 +0200

    add

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,34 @@
-import { useEffect } from 'react'
-import Head from 'next/head';
 import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
+import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
-import { Loader } from '../components/organisms/Loader';
-import { inter, maloca, orbitron } from '../utils/fonts';
-import Smartlook from 'smartlook-client'
 
 export default function Index({ allPosts, preview }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
-
-
-
-  useEffect(() => {
-    // Initialize Smartlook with URL interceptor
-    Smartlook.init('b8d3218cfb5c3b4885eb5f77d8143ffef0ca4e4d',
-      undefined,
-      () => console.log('Smartlook is now initialized'), {
-      region: 'eu',
-      interceptors: {
-        url: (data) => {
-          const tokenIndex = data.url.indexOf('tokenId=');
-          if (tokenIndex > -1) {
-            data.url = data.url.slice(0, tokenIndex) + 'tokenId=[OBSCURED]';
-
-          }
-        }
-      }
-    });
-  }, []);
-
   return (
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Welcome to {CMS_NAME}</title>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
-        {/* <style style jsx global> {`
-        :root {
-          --orbitron-font: ${orbitron.style.fontFamily};
-          --inter-font: ${inter.style.fontFamily};
-          --maloca-font: ${maloca.style.fontFamily};
-        }
-      `}</style > */}
         <Container>
-          <Loader />
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
